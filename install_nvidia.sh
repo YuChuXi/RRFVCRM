36d914fbd486758244d6a0b9832d59527ed19774
alic-li <2591709191@qq.com>
2024-07-31 22:23:00 +0800

    fix chat bug

--- install_nvidia.sh
+++
@@ -1,5 +0,0 @@
-python --version
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-pip install torch==2.2.1+cu118 torchvision==0.17.1+cu118 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html
--- run_gui.sh
+++
@@ -1,2 +0,0 @@
-source venv/bin/activate
-python webui.py
